f49ef16f3a42c20005c0823ffbf5319ed3934ede
Francis Ngo <francis.t.ngo@gmail.com>
2023-06-20 11:06:02 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CMS Capture - A CMS Tool for Capture Marketplace
+
+CMS Capture is a robust Content Management System built for the E-Commerce Website, Capture. It provides a user-friendly interface for managing the content and data of the products on the website. 
---
+++ server/src/index.ts
@@ -0,0 +1,28 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { gql } from 'graphql-tag';
+
+const typeDefs = gql`
+    type Query {
+        hello: String!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => 'world'
+    },
+};
+
+(async () => {
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+
+    const { url } = await startStandaloneServer(server, {
+        listen: { port: 4000 }
+    });
+
+    console.log(`🚀 Server ready at http://localhost:4000`);
+})();
