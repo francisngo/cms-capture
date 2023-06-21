7487236ce8f986746afbad1788a3af9700cd10b9
Francis Ngo <francis.t.ngo@gmail.com>
2023-06-21 13:15:52 -0700

    Add express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,15 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import http from 'http';
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
 import { gql } from 'graphql-tag';
+
+const app = express();
+app.use(cors({ origin: 'http://localhost:8080' }));
+app.use(morgan('dev'));
+app.use(express.json());
+const httpServer = http.createServer(app);
 
 const typeDefs = gql`
     type Query {
@@ -15,14 +24,18 @@ const resolvers = {
 };
 
 (async () => {
+    app.get('/', (req, res) => {
+        res.json({ hello: 'world' });
+    })
+
     const server = new ApolloServer({
         typeDefs,
         resolvers,
     });
+    await server.start();
 
-    const { url } = await startStandaloneServer(server, {
-        listen: { port: 8080 }
-    });
+    const PORT = process.env.PORT || 8080;
+    await new Promise<void>((resolve) => httpServer.listen({ port: PORT }, resolve));
 
     console.log(`🚀 Server ready at http://localhost:8080`);
 })();
